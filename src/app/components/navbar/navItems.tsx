04d1b460a70a9777e18101f4fff235357979fdd8
devZ_VikAsH <pvickey663@gmail.com>
2022-08-06 17:25:57 +0530

    started gql

---
+++ src/app/components/navbar/navItems.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { slide as Menu } from "react-burger-menu";
+import tw from "twin.macro";
+import { useMediaQuery } from "react-responsive";
+import {SCREENS} from "../responsive"
+
+const ListContainer = styled.ul`
+  ${tw`
+    flex
+    list-none
+  `};
+`;
+
+// const NavItem = styled.li<{ menu?: any }>`
+const NavItem = styled.li`
+  ${tw`
+    text-sm
+    md:text-base
+    text-black
+    font-medium
+    mr-1
+    md:mr-5
+    cursor-pointer
+    transition
+    duration-300
+    ease-in-out
+    hover:text-gray-700
+  `};
+`;
+
+export function NavItems() {
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+  return (
+    <ListContainer>
+      <NavItem>
+        <a href="#">Home</a>
+      </NavItem>
+      <NavItem>
+        <a href="#">Cars</a>
+      </NavItem>
+      <NavItem>
+        <a href="#">Services</a>
+      </NavItem>
+      <NavItem>
+        <a href="#">Contact Us</a>
+      </NavItem>
+    </ListContainer>
+  );
+}
