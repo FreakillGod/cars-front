d4ff6c6bf5641e37412738be83b15b5af910c62f
devZ_VikAsH <pvickey663@gmail.com>
2022-08-14 21:12:26 +0530

    Added Footer

--- src/app/containers/index.tsx
+++ src/app/containers/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import BookCard from "../components/bookCard";
+import Footer from "../components/footer";
 import { Marginer } from "../components/marginer";
 import NavContainer from "../components/navbar";
 import AboutUs from "./HomePage/AboutUs";
@@ -33,6 +34,7 @@ export function HomePage() {
       <AboutUs />
       <Marginer direction="vertical" margin="5em" />
       <TopCards />
+      <Footer/>
     </PageContainer>
   );
 }
